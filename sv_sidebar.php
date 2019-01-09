99a3dd940594dc59507e7e856d7343b0398adbad
Adrian Chudzynski <adrian-chudzynsk@straightvisions.com>
2019-01-09 21:39:23 +0100

    Refactored

--- sv_sidebar.php
+++ sv_sidebar.php
@@ -11,18 +11,29 @@ namespace sv_100;
  */
 
 class sv_sidebar extends init {
-	static $scripts_loaded						= false;
 	private $template                           = false;
 	private $sidebars                           = array();
 
 	public function __construct() {
 
 	}
+
 	public function init(){
-		add_shortcode( $this->get_module_name(), array( $this, 'shortcode' ) );
-		
+		// Module Info
+		$this->set_module_title( 'SV Sidebar' );
+		$this->set_module_desc( __( 'This module gives the ability to display sidebars via the "[sv_sidebar]" shortcode.', $this->get_module_name() ) );
+
+		// Load Styles
+		static::$scripts->create( $this )
+			->set_source( $this->get_file_url( 'lib/css/frontend.css' ), $this->get_file_path( 'lib/css/frontend.css' ) );
+
+		// Action Hooks
 		add_action( 'widgets_init', array( $this, 'sidebars' ) );
+
+		// Shortcodes
+		add_shortcode( $this->get_module_name(), array( $this, 'shortcode' ) );
 	}
+
 	public function shortcode( $settings, $content = '' ) {
 		$settings								= shortcode_atts(
 			array(
@@ -35,8 +46,6 @@ class sv_sidebar extends init {
 
 		$this->template                         = $settings['template'] ? $settings['template'] : 'home';
 
-		$this->module_enqueue_scripts( $settings['inline'] );
-
 		ob_start();
 		include( $this->get_file_path( 'lib/tpl/frontend.php' ) );
 		$output									= ob_get_contents();
@@ -48,85 +57,85 @@ class sv_sidebar extends init {
 	public function sidebars() {
 		array_push( $this->sidebars,
 			array(
-				'name'									=> __( 'Footer - Left', $this->get_module_name() ),
-				'id'									=> $this->get_module_name() . '_footer_left',
-				'description'							=> __( 'Widgets in this area will be shown in the left section of the footer.', $this->get_module_name() ),
-				'before_widget'							=> '<div id="%1$s" class="widget %2$s mb-3">',
-				'after_widget'							=> '</div>',
-				'before_title'							=> '<h3 class="' . $this->get_module_name() . '">',
-				'after_title'							=> '</h3>',
+				'name'							=> __( 'Footer - Left', $this->get_module_name() ),
+				'id'							=> $this->get_module_name() . '_footer_left',
+				'description'					=> __( 'Widgets in this area will be shown in the left section of the footer.', $this->get_module_name() ),
+				'before_widget'					=> '<div id="%1$s" class="widget %2$s mb-3">',
+				'after_widget'					=> '</div>',
+				'before_title'					=> '<h3 class="' . $this->get_module_name() . '">',
+				'after_title'					=> '</h3>',
 			)
 		);
 
 		array_push( $this->sidebars,
 			array(
-				'name'									=> __( 'Footer - Center', $this->get_module_name() ),
-				'id'									=> $this->get_module_name() . '_footer_center',
-				'description'							=> __( 'Widgets in this area will be shown in the center section of the footer.', $this->get_module_name() ),
-				'before_widget'							=> '<div id="%1$s" class="widget %2$s mb-3">',
-				'after_widget'							=> '</div>',
-				'before_title'							=> '<h3 class="' . $this->get_module_name() . '">',
-				'after_title'							=> '</h3>',
+				'name'							=> __( 'Footer - Center', $this->get_module_name() ),
+				'id'							=> $this->get_module_name() . '_footer_center',
+				'description'					=> __( 'Widgets in this area will be shown in the center section of the footer.', $this->get_module_name() ),
+				'before_widget'					=> '<div id="%1$s" class="widget %2$s mb-3">',
+				'after_widget'					=> '</div>',
+				'before_title'		    		=> '<h3 class="' . $this->get_module_name() . '">',
+				'after_title'					=> '</h3>',
 			)
 		);
 
 		array_push( $this->sidebars,
 			array(
-				'name'									=> __( 'Footer - Right', $this->get_module_name() ),
-				'id'									=> $this->get_module_name() . '_footer_right',
-				'description'							=> __( 'Widgets in this area will be shown in the right section of the footer.', $this->get_module_name() ),
-				'before_widget'							=> '<div id="%1$s" class="widget %2$s mb-3">',
-				'after_widget'							=> '</div>',
-				'before_title'							=> '<h3 class="' . $this->get_module_name() . '">',
-				'after_title'							=> '</h3>',
+				'name'							=> __( 'Footer - Right', $this->get_module_name() ),
+				'id'							=> $this->get_module_name() . '_footer_right',
+				'description'					=> __( 'Widgets in this area will be shown in the right section of the footer.', $this->get_module_name() ),
+				'before_widget'					=> '<div id="%1$s" class="widget %2$s mb-3">',
+				'after_widget'				    => '</div>',
+				'before_title'					=> '<h3 class="' . $this->get_module_name() . '">',
+				'after_title'					=> '</h3>',
 			)
 		);
 
 		array_push( $this->sidebars,
 			array(
-				'name'									=> __( 'Home Sidebar', $this->get_module_name() ),
-				'id'									=> $this->get_module_name() . '_home',
-				'description'							=> __( 'Widgets in this area will be shown in the sidebar of the home page.', $this->get_module_name() ),
-				'before_widget'							=> '<div id="%1$s" class="widget %2$s mb-3">',
-				'after_widget'							=> '</div>',
-				'before_title'							=> '<h3 class="' . $this->get_module_name() . '">',
-				'after_title'							=> '</h3>',
+				'name'							=> __( 'Home Sidebar', $this->get_module_name() ),
+				'id'							=> $this->get_module_name() . '_home',
+				'description'					=> __( 'Widgets in this area will be shown in the sidebar of the home page.', $this->get_module_name() ),
+				'before_widget'					=> '<div id="%1$s" class="widget %2$s mb-3">',
+				'after_widget'					=> '</div>',
+				'before_title'					=> '<h3 class="' . $this->get_module_name() . '">',
+				'after_title'					=> '</h3>',
 			)
 		);
 
 		array_push( $this->sidebars,
 			array(
-				'name'									=> __( 'Post Sidebar', $this->get_module_name() ),
-				'id'									=> $this->get_module_name() . '_post',
-				'description'							=> __( 'Widgets in this area will be shown in the sidebar of single posts.', $this->get_module_name() ),
-				'before_widget'							=> '<div id="%1$s" class="widget %2$s mb-5">',
-				'after_widget'							=> '</div>',
-				'before_title'							=> '<h3 class="' . $this->get_module_name() . ' mb-4 pb-3 border-bottom">',
-				'after_title'							=> '</h3>',
+				'name'							=> __( 'Post Sidebar', $this->get_module_name() ),
+				'id'							=> $this->get_module_name() . '_post',
+				'description'					=> __( 'Widgets in this area will be shown in the sidebar of single posts.', $this->get_module_name() ),
+				'before_widget'					=> '<div id="%1$s" class="widget %2$s mb-5">',
+				'after_widget'					=> '</div>',
+				'before_title'					=> '<h3 class="' . $this->get_module_name() . ' mb-4 pb-3 border-bottom">',
+				'after_title'					=> '</h3>',
 			)
 		);
 
 		array_push( $this->sidebars,
 			array(
-				'name'									=> __( 'Author Sidebar', $this->get_module_name() ),
-				'id'									=> $this->get_module_name() . '_author',
-				'description'							=> __( 'Widgets in this area will be shown in the sidebar of author pages.', $this->get_module_name() ),
-				'before_widget'							=> '<div id="%1$s" class="widget %2$s mb-5">',
-				'after_widget'							=> '</div>',
-				'before_title'							=> '<h3 class="' . $this->get_module_name() . ' mb-4 pb-3 border-bottom">',
-				'after_title'							=> '</h3>',
+				'name'							=> __( 'Author Sidebar', $this->get_module_name() ),
+				'id'							=> $this->get_module_name() . '_author',
+				'description'					=> __( 'Widgets in this area will be shown in the sidebar of author pages.', $this->get_module_name() ),
+				'before_widget'					=> '<div id="%1$s" class="widget %2$s mb-5">',
+				'after_widget'					=> '</div>',
+				'before_title'					=> '<h3 class="' . $this->get_module_name() . ' mb-4 pb-3 border-bottom">',
+				'after_title'					=> '</h3>',
 			)
 		);
 
 		array_push( $this->sidebars,
 			array(
-				'name'									=> __( 'Search Sidebar', $this->get_module_name() ),
-				'id'									=> $this->get_module_name() . '_search',
-				'description'							=> __( 'Widgets in this area will be shown in the sidebar of search page.', $this->get_module_name() ),
-				'before_widget'							=> '<div id="%1$s" class="widget %2$s mb-5">',
-				'after_widget'							=> '</div>',
-				'before_title'							=> '<h3 class="' . $this->get_module_name() . ' mb-4 pb-3 border-bottom">',
-				'after_title'							=> '</h3>',
+				'name'							=> __( 'Search Sidebar', $this->get_module_name() ),
+				'id'							=> $this->get_module_name() . '_search',
+				'description'					=> __( 'Widgets in this area will be shown in the sidebar of search page.', $this->get_module_name() ),
+				'before_widget'					=> '<div id="%1$s" class="widget %2$s mb-5">',
+				'after_widget'					=> '</div>',
+				'before_title'					=> '<h3 class="' . $this->get_module_name() . ' mb-4 pb-3 border-bottom">',
+				'after_title'					=> '</h3>',
 			)
 		);
 
